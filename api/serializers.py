a44d3343424616d455eeb01fcb00dd1097a3c49e
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-04-22 17:24:11 +0530

    first commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = '__all__'
+        
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        
+        
+    def save(self, **kwargs):
+        new_user = User.objects.create_user(
+            username= self.validated_data['username'],
+            email= self.validated_data['email'],
+            password= self.validated_data['password']
+        )
+        
+        new_user.save()
+        
+        new_token = Token.objects.create(user=new_user)
--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 from rest_framework import status
 from api.models import *
 from api.serializers import *
+from rest_framework import status
+
 # Create your views here.
 
 
@@ -13,3 +15,32 @@ def car_list(request):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+class Signup(APIView):
+    def post(self, request, Fomat=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            user = User.objects.get(username=request.data[' username'])
+            token = Token.objects.get(user=user)
+            
+            serializer = UserSerializer(user)
+            
+            data = {
+                "user": serializer.data,
+                "token": token
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
+    
+class Login(APIView):
+    def post(self, request, Fomat=None):
+        pass
+    
+class TestView(APIView):
+    def get(self, request, Fomat=None):
+        pass
